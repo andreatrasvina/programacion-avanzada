112a3d7b9f88dc16b6836b180eb52bf997472241
Yeya <yeya@MacBook-Air-de-Hazael.local>
2024-09-11 09:51:47 -0700

    VUE

---
+++ vue-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import trabajo5 from './components/trabajo5.vue'
+</script>
+
+<template>
+    <trabajo5></trabajo5>
+    
+
+</template>
+
