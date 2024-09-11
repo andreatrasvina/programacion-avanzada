8958604b24f74871ba078467c1a3e5455470e3cb
andreatrasvina <alucerotrasvina@gmail.com>
2024-09-11 12:45:47 -0700

    re do

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import trabajo5 from './components/trabajo5.vue'
+</script>
+
+<template>
+    <trabajo5></trabajo5>
+
+</template>
+
