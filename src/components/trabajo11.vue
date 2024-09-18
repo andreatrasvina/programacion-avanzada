20012c41577dc3338fc5671835dace2c9077f931
Andrea Lucero <alucerotrasvina@gmail.com>
2024-09-18 00:05:59 -0700

    Tarea 12

--- src/components/trabajo11.vue
+++ src/components/trabajo11.vue
@@ -6,7 +6,25 @@
       <button type="submit">Access</button>
     </form>
 
-    <p v-if="message">{{ message }}</p>
+    <p v-if="message">{{ message }} {{ email }}</p>
+
+    <table v-if="isAuthenticated">
+      
+      <thead>
+        <tr>
+          <th>Email</th>
+          <th>Password</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="user in users" :key="user.email">
+          <td>{{ user.email }}</td>
+          <td>{{ user.password }}</td>
+        </tr>
+      </tbody>
+
+    </table>
 
   </div>
 </template>
@@ -19,7 +37,18 @@ export default {
       password: '',
       message: '',
       isAuthenticated: false,
+      users: [],
     };
+  },
+
+  created() {
+    //verifica si hay usuario enel localStorage
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      this.isAuthenticated = true;
+      this.users = JSON.parse(storedUser); //recupera el usuario
+      this.message = "SESION GUARDADA!!!";
+    }
   },
 
   methods: {
@@ -29,12 +58,17 @@ export default {
         const users = await response.json();
 
         const validUser = users.some(user =>
-          user.email === this.email && user.password === this.password
+        user.email === this.email && user.password === this.password
         );
 
         if (validUser) {
           this.message = "Bienvenido";
           this.isAuthenticated = true; 
+          this.users = users;
+          
+          localStorage.setItem('user', JSON.stringify(users));
+          //sessionStorage.setItem('users', JSON.stringify(users));
+
         } else {
           alert("No existe el usuario");
           this.message = '';
@@ -46,3 +80,15 @@ export default {
   }
 };
 </script>
+
+<style>
+
+table{
+  background-color: pink;
+}
+
+th{
+  background-color: cornsilk;
+}
+
+</style>
